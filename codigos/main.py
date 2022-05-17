e6b91eb56186485b0331d75c3c369588949f6294
Matheus M. Parrela <matheusparrela13@gmail.com>
2022-05-17 16:21:07 -0300

    Atualização
    Gráfico

--- codigos/main.py
+++ codigos/main.py
@@ -1,10 +1,15 @@
 #OTIMIZAÇÃO NÃO LINEAR - MÉTODOS DOS GRADIENTES
 import sympy as sy
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
 
 sy.init_printing()
 
 #Condições Iniciais
-x = 1/3
+w = 1/3
 y = 5/2
 
 x1 = sy.Symbol('x1')
@@ -25,13 +30,13 @@ while i < 5:
     grad1 = sy.diff(funcao, x1)         #Gradiente da Função
     grad2 = sy.diff(funcao, x2)
     
-    a = grad1.subs(x1, x)               #Valor de X no gradiente
+    a = grad1.subs(x1, w)               #Valor de X no gradiente
     a2 = a.subs(x2, y)
 
-    b = grad2.subs(x1, x)               #Valor de Y no gradiente
+    b = grad2.subs(x1, w)               #Valor de Y no gradiente
     b2 = b.subs(x2, y)
    
-    y1 = (x - alfa*a2)                  #Expressão do alfa
+    y1 = (w - alfa*a2)                  #Expressão do alfa
     y2 = (y - alfa*b2)
 
     funcao1 = funcao.subs(x1, y1)       #Substituição de de y1 e y2 na função 
@@ -47,5 +52,26 @@ while i < 5:
     print('Ponto X1:',resultado1,'\nPonto X2:',resultado2)
    
     i= i + 1                            #Atualiza os valores de x e y
-    x = resultado1
+    w = resultado1
     y = resultado2
+
+#Plot do Gráfico da Função
+def f(x1, x2):  
+        ar = ((x1 + 2*x2 - 7)**2 + (2*x1 + x2 - 5)**2)   
+        return ar
+
+x = np.linspace(-6, 6, 30)
+y = np.linspace(-6, 6, 30)
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+fig = plt.figure(figsize=(10, 10))
+ax = plt.axes(projection='3d')
+ax.contour3D(X, Y, Z, 50, cmap='binary')
+ax = plt.axes(projection='3d')
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1,cmap='winter', edgecolor='none')
+ax.set_title('Gráfico de superfície no Espaço 3D', fontsize=18);
+ax.set_xlabel('Eixo X', fontsize=15)
+ax.set_ylabel('Eixo Y', fontsize=15)
+ax.set_zlabel('Eixo Z', fontsize=15)
+ax.view_init(60, 45)
+plt.show()

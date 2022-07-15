daa0f05f066cbce8266949714f90209b540529b5
Matheus M. Parrela <92748188+matheusparrela@users.noreply.github.com>
2022-07-14 22:50:38 -0300

    Atualização
    Integração com o arquivo search_result

--- codigos/metodo_newton.py
+++ codigos/metodo_newton.py
@@ -1,30 +1,38 @@
 #OTIMIZAÇÃO NÃO LINEAR - MÉTODOS DE NEWTON
 import sympy as sy
 import solucao_grafica as sg
+import search_result as sr
 
-def newton(): 
+def newton(function, x, y, precision_decimals): 
   
     sy.init_printing(use_latex='png', scale=1.05, order='grlex',forecolor='Black', backcolor='White', fontsize=10)
 
-    #Condições Iniciais
-    x = 3/4
-    y = 1/4
-    resultado1 = 0
-    resultado2 = 0
+    points = []
+    points.append([x,y])
+
+    converged = True
 
     pontos = []
     iteracoes = []
     lista_x = []
     lista_y = []
+    k = 0
 
-    symbols = sy.symbols("x1 x2")
-    entrada_funcao =  "((x1 - 3)**2 + (x2 - 2)**2)"
-    
-    expression = sy.parsing.sympy_parser.parse_expr(entrada_funcao)
+    iteracoes.append(k)
+    lista_x.append(x)
+    lista_y.append(y)
+
+    symbols = sy.symbols("x1 x2")    
+    expression = sy.parsing.sympy_parser.parse_expr(function)
     
     grad = []
     grad.append(sy.diff(expression, symbols[0]))        #Gradiente da Função
     grad.append(sy.diff(expression, symbols[1]))
+
+    h11 = sy.diff(grad[0], symbols[0])       #Calcula os elementos da hessiana
+    h12 = sy.diff(grad[0], symbols[1])
+    h21 = sy.diff(grad[1], symbols[0])
+    h22 = sy.diff(grad[1], symbols[1])
 
     for k in range (0,5):
 
@@ -34,11 +42,7 @@ def newton():
         b = grad[1].subs(symbols[0], x)               #Valor de Y no gradiente
         b2 = b.subs(symbols[1], y)
 
-        h11 = sy.diff(grad[0], symbols[0])       #Calcula os elementos da hessiana
-        h12 = sy.diff(grad[0], symbols[1])
-        h21 = sy.diff(grad[1], symbols[0])
-        h22 = sy.diff(grad[1], symbols[1])
-                                            #Passa as condições iniciais, elementos do hesiano e o gradiente no pontos iniciais para matrizes
+        #Passa as condições iniciais, elementos do hesiano e o gradiente no pontos iniciais para matrizes
         P = sy.Matrix([[x], [y]])            
         H = sy.Matrix([[h11, h12], [h21, h22]])
         G = sy.Matrix([[a2], [b2]])
@@ -49,21 +53,19 @@ def newton():
 
         except:
             print("Hessina não Invertivel")
+            converged = False
             break
 
-        if (resultado1 != x0[0,0] or resultado2 != x0[1,0]) and k != 0:
+        if (x != x0[0,0] or y != x0[1,0]) and k != 0:
             print("Erro - Função não quadrática")
+            converged = False
             break
 
-        resultado1 = x0[0,0]           #Retira os resultados da matriz
-        resultado2 = x0[1,0]
-        x = resultado1
-        y = resultado2
+        x = round(x0[0,0], precision_decimals)           #Retira os resultados da matriz
+        y = round(x0[1,0], precision_decimals)
 
-        print('\nMétodo de Iteração única:')
-        print('\nResultado:', resultado1,'\n', resultado2)
-
-        iteracoes.append(k)
+        iteracoes.append(k+1)
+        points.append([x,y])
         lista_x.append(x)
         lista_y.append(y)
     
@@ -74,7 +76,10 @@ def newton():
         sg.plot_convergencia(iteracoes, pontos)
         sg.plot_curvasniveis(expression, symbols, lista_x, lista_y)
         sg.grafico_3d(expression, symbols)
-        #sg.deslocamento_3d(expression, symbols, lista_x, lista_y)
         #sg.grafico_misto(expression, symbols, lista_x, lista_y, pontos)
 
-newton()
+    return sr.SearchResult(function, points, converged)
+
+result = newton("((x1 - 3)**2 + (x2 - 2)**2)", 3/4, 1/4, precision_decimals=4)
+
+print(result)
--- codigos/metodo_newton_modificado.py
+++ codigos/metodo_newton_modificado.py
@@ -1,34 +1,32 @@
 #OTIMIZAÇÃO NÃO LINEAR - MÉTODOS DE NEWTON MODIFICADO   
 import sympy as sy
 import solucao_grafica as sg
+import search_result as sr
 
-def modificado(): 
+def modificado(function, x, y, precision_decimals=4): 
   
     sy.init_printing(use_latex='png', scale=1.05, order='grlex',forecolor='Black', backcolor='White', fontsize=10)
 
-    #Condições Iniciais
-    x = 0.25
-    y = -0.8
+    points = []
+    points.append([x,y])
+    converged = True
 
-    precisao = 0.001
+    precision = 10**(-precision_decimals)
 
     alfa = sy.symbols("alfa")
-
     symbols = sy.symbols("x1 x2")
-    entrada_funcao = "2*x1**2 - 1.05*x1**4 + ((x1**6)/6) +  x1*x2 + x2**2"
-    entrada_funcao = "(4 - 2.1*x1**2 + (x1**4)/3)*x1**2 + x1*x2 + (-4 +4*x2**2)*x2**2"
-    entrada_funcao ="(x1-1)**2 + 2*((2*x2**2 - x1)**2)"
-    expression = sy.parsing.sympy_parser.parse_expr(entrada_funcao)
-        
-    
-    resultado1 = 0
-    resultado2 = 0
+    expression = sy.parsing.sympy_parser.parse_expr(function)
+          
     k = 0
     laco = True
     pontos = []
     iteracoes = []
     lista_x = []
     lista_y = []
+
+    iteracoes.append(k)
+    lista_x.append(x)
+    lista_y.append(y)
 
     grad1 = sy.diff(expression, symbols[0])         #Gradiente da Função
     grad2 = sy.diff(expression, symbols[1])
@@ -39,24 +37,20 @@ def modificado():
     h22 = sy.diff(grad2, symbols[1])
 
     #Passa as condições iniciais, elementos da hessiana e o gradiente no pontos iniciais para matrizes
-
     hessiana = sy.Matrix([[h11, h12],[h21, h22]]) 
-
     gradiente = sy.Matrix([[grad1], [grad2]])
 
     try:
         inv_hessiana = -hessiana.inv()       #Inverte a matriz hessiana
         prod = inv_hessiana*gradiente
         
-
     except:
         print("Hessiana não Inversível")
+        converged = False
         laco = False
          
-
     while laco == True:
         
-
         prod1 = prod.subs(symbols[0], x)
         prod2 = prod1.subs(symbols[1], y)
 
@@ -66,9 +60,8 @@ def modificado():
         funcao1 = expression.subs(symbols[0], y1)       #Substituição de de y1 e y2 na função 
         funcao2 = funcao1.subs(symbols[1], y2)
        
-        d1 = sy.diff(funcao2, alfa)                     #Derivada da função em relação a alfa
-
-        raiz = (sy.solve(d1))
+        df_dalfa = sy.diff(funcao2, alfa)                     #Derivada da função em relação a alfa
+        raiz = (sy.solve(df_dalfa))
        
         try: 
             raiz = raiz[0]
@@ -84,22 +77,16 @@ def modificado():
             if grad1 == 0 and grad2 == 0:
                     k = k+1
                     print("Gradiente: Zero")
-                    print('ITERAÇÃO',k,":")
-                    print('Ponto X1:',x,'\nPonto X2:',y,'\n')
                     lista_x.append(x)
                     lista_y.append(y)
                     iteracoes.append(k)
                     k = 5
                     break
+               
+        x = round(y1.subs(alfa, raiz), precision_decimals)                #Substitui o valor da raiz de alfa na expressão de alfa
+        y = round(y2.subs(alfa, raiz), precision_decimals)
         
-        
-        resultado1 = y1.subs(alfa, raiz)                #Substitui o valor da raiz de alfa na expressão de alfa
-        resultado2 = y2.subs(alfa, raiz)
-        
-                         
-        x = round(resultado1,4)                         #Atualiza os valores de x e y
-        y = round(resultado2,4)  
-        
+        points.append([x,y])
         lista_x.append(x)
         lista_y.append(y)
         pontos = sg.funcao(expression, symbols, lista_x, lista_y)
@@ -107,34 +94,26 @@ def modificado():
         k = k + 1
         iteracoes.append(k)
         
-
-        print('\nITERAÇÃO',k,":")
-        print('Ponto X1:',x,'\nPonto X2:',y)
-
-        
         if k >= 5:                                      #Critério de Parada 2
          
             fmax = max(pontos[k-5:])
             fmin = min(pontos[k-5:])
 
-            if abs(fmax - fmin) < precisao:
+            if abs(fmax - fmin) < precision:
                 laco = False
-
 
     pontos = sg.funcao(expression, symbols, lista_x, lista_y)
     
     if k > 4:
+        
         #Plote dos Gráficos
         #sg.grafico_misto(expression, symbols, lista_x, lista_y, pontos)
         sg.plot_convergencia(iteracoes, pontos)
         sg.plot_curvasniveis(expression, symbols, lista_x, lista_y)
-        #sg.grafico_3d1(expression, symbols, lista_x, lista_y, pontos)
         sg.grafico_3d(expression, symbols)
-        #sg.deslocamento_3d(expression, symbols, lista_x, lista_y)
 
+    return sr.SearchResult(function, points, converged)
 
-modificado()
+result = modificado("(x1-1)**2 + 2*((2*x2**2 - x1)**2)", 0.25, -0.8, precision_decimals=4)
 
-
-
-
+print(result)
